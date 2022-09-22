9466a3583d464455524b1262bb8963f087fe5cb3
Mohamed_elg <mohamed.elguermat9@gmail.com>
2022-09-22 14:07:33 +0200

    22/09/22

--- modification_zip.py
+++ modification_zip.py
@@ -1,38 +1,28 @@
 #!/bin/python3
 
 from unittest import result
-import urllib.request
-import zipfile
 
-#import pour la comparaison de fichier sql
+# import pour la comparaison de fichier sql
 from difflib import Differ
-from pprint import pprint 
+from pprint import pprint
 
-# Récupération de l'archive zip
-#!Adapter l'adresse IP en fonction du réseau
-# url = 'http://192.168.23.226/test_export.sql.zip'
-# urllib.request.urlretrieve(url, 'test_export.zip')
-
-# Décompression de l'archive
-# Pas d'argument pour extractall --> dans le répertoire courant
-# zipfile.ZipFile('test_export.zip', 'r').extractall()
-
-d = Differ() 
-result = list(d.compare(open('test100.sql', 'r').readlines(), open('test100-copy.sql', 'r').readlines()))
+d = Differ()
+result = list(d.compare(open('test100.sql', 'r').readlines(),
+              open('test100-copy.sql', 'r').readlines()))
 pprint(result)
 
-v = ['voiture','maison']
+v = ['voiture', 'maison']
 
 print(v.index('voiture'))
- 
+
+
 def modification(result):
     for line in result:
         if ('+' in line):
             print("find a modification")
-            return(True)
+            return (True)
     print("no modification found")
-    return(False)
+    return (False)
+
 
 modification(result)
-
-
