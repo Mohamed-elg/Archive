1156d1f4a26b9b792393b1fe52b26913803f2de9
Mohamed_elg <mohamed.elguermat9@gmail.com>
2022-09-22 11:39:43 +0200

    url 22/09/22

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 #!/bin/python3
-#test
+
+import urllib.request
+
+
+# Récupération de l'archive zip
+#!Adapter l'adresse IP en fonction du réseau
+url = 'http://192.168.23.226/test_export.sql.zip'
+urllib.request.urlretrieve(url, 'test_export.zip')
+
+# Décompression de l'archive
