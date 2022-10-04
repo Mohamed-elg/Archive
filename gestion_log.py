5aa3936ad115d87591dc24e9b62840806188ad4f
Mohamed_elg <mohamed.elguermat9@gmail.com>
2022-10-04 14:21:17 +0200

    04/10/2022

--- gestion_log.py
+++ gestion_log.py
@@ -1,13 +1,7 @@
 #!/bin/python3
 
-import os
 from datetime import datetime
 from time import strftime
-
-
-def creer_rapport():
-    now = datetime.now()
-    os.system("touch logs/"+now.strftime("%Y-%m-%d")+".log")
 
 
 def Ecrire_rapport(msg):
